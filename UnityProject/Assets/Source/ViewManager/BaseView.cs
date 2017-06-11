f714f3749dd385462a9aeca9817604fe87dd1635
William Vickery <william@vickery.com>
2017-06-10 20:17:02 -0500

    Updated gitignore, added DOTWeen, created first scene, starte view manager implementation

---
+++ UnityProject/Assets/Source/ViewManager/BaseView.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseView : MonoBehaviour
+{
+	#region API
+	public void Open(ViewManager InViewManager)
+	{
+		if (m_ViewManager == null)
+		{
+			m_ViewManager = InViewManager;
+			StartCoroutine(Show());
+		}
+	}
+
+	public void Close()
+	{
+		if (m_ViewManager != null)
+		{
+			StartCoroutine(Hide());
+			m_ViewManager = null;
+		}
+	}
+
+	public IEnumerator Show(bool bInstant = false)
+	{
+		if (!m_bShown)
+		{
+			m_ShowCount++;
+			PreShow(m_ShowCount == 1);
+
+			if (!bInstant)
+			{
+				yield return StartCoroutine(AnimateIn());
+			}
+
+			OnShow();
+		}
+	}
+
+	public IEnumerator Hide(bool bInstant = false)
+	{
+		if (m_bShown)
+		{
+			OnHide();
+
+			if (!bInstant)
+			{
+				yield return StartCoroutine(AnimateOut());
+			}
+
+			PostHide();
+		}
+	}
+	#endregion
+
+	#region View Interface
+	protected virtual void PreShow(bool bFirstTime) { }
+	protected virtual IEnumerator AnimateIn() { yield return null; }
+	protected virtual void OnShow() { }
+
+	protected virtual void OnHide() { }
+	protected virtual void PostHide() { }
+
+	protected virtual IEnumerator AnimateOut() { yield return null; }
+	#endregion
+
+	private ViewManager m_ViewManager = null;
+
+	private int m_ShowCount = 0;
+	private bool m_bShown = false;
+}
---
+++ UnityProject/Assets/Source/ViewManager/ViewManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseViewTracker : CustomYieldInstruction
+{
+	public override bool keepWaiting
+	{
+		get
+		{
+			return !m_bDone;
+		}
+	}
+
+	public BaseViewTracker(BaseView InTargetView)
+	{
+		m_TargetView = InTargetView;
+	}
+
+	public void SetComplete()
+	{
+		m_bDone = true;
+	}
+
+	public TViewType GetView<TViewType>()
+		where TViewType : BaseView
+	{
+		return m_TargetView as TViewType;
+	}
+
+	private bool m_bDone = false;
+	private BaseView m_TargetView;
+
+}
+
+public class ViewManager : MonoBehaviour
+{
+	#region Inspector Properties
+
+	public List<BaseView> m_ViewPrefabs;
+
+	#endregion
+
+	#region API
+
+	public BaseViewTracker ShowView<TViewType>(string PrefabName, bool bHidePrevious = true)
+		where TViewType : BaseView
+	{
+		if (m_RootCanvas != null)
+		{
+			foreach (BaseView ViewPrefab in m_ViewPrefabs)
+			{
+				if (ViewPrefab.gameObject.name == PrefabName)
+				{
+					if (ViewPrefab is TViewType)
+					{
+						BaseView NewView = Instantiate<BaseView>(ViewPrefab, m_RootCanvas.transform);
+						BaseViewTracker NewViewTracker = new BaseViewTracker(NewView);
+
+						m_ShowQueue.Enqueue(NewViewTracker);
+
+						return NewViewTracker;
+					}
+					else
+					{
+						throw new ArgumentException(String.Format("ShowView Type Mismatch.Prefab[{0}] Expected[{1}], Given[{2}]", PrefabName, typeof(TViewType).ToString(), ViewPrefab.GetType().ToString()));
+					}
+				}
+			}
+		}
+		else
+		{
+			throw new NullReferenceException("Root Canvas is null.");
+		}
+
+		return null;
+	}
+
+	#endregion
+
+	private Canvas m_RootCanvas = null;
+
+	private Queue<BaseViewTracker> m_ShowQueue = new Queue<BaseViewTracker>();
+	private Stack<BaseViewTracker> m_ViewStack = new Stack<BaseViewTracker>();
+}
